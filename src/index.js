03e05c56b391f45e96f9fd6ea5cb79363bd44f91
Vincenzo Mann <vinnymann97@gmail.com>
2019-03-07 18:43:27 +0000

    hosted

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import './NavBar.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './store/reducers/rootReducer';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
-import fbConfig from './config/fbConfig';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+// import './NavBar.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
 
 const allStoreEnhancers = compose(
   applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
